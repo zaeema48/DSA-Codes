843630848bbcf81eedcf87830cf79632e4cc4dad
Zaeema <zaeemakhanam2001@gmail.com>
2022-06-15 23:55:21 +0530

    Doubly Linked List

---
+++ DoublyLinkedList.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+
+struct node{
+    struct node *prev;
+    int data;
+    struct node *next;
+};
+struct node *head=NULL;
+
+void add(int value){
+    struct node *temp=(struct node*)malloc(sizeof(struct node));
+    temp->prev=NULL;
+    temp->data=value;
+    temp->next=NULL;
+    head=temp;
+    
+}
+void beginning(int value){
+    struct node *temp=(struct node*)malloc(sizeof(struct node)); 
+    temp->data=value;
+    temp->next=head; 
+    head->prev=temp; 
+    head=temp;
+    
+}
+void InsertEnd(int value){
+    struct node *tp=head;
+    struct node *temp=(struct node*)malloc(sizeof(struct node)); 
+    temp->data=value;
+    temp->next=NULL; 
+    while(tp->next!=NULL) 
+    tp=tp->next;
+    tp->next=temp;
+    temp->prev=tp;
+    
+}
+void AtPos(int value, int pos){
+    struct node *temp=(struct node*)malloc(sizeof(struct node));
+    temp->prev=NULL;
+    temp->data=value;
+    temp->next=NULL;
+    struct node *ptr1=head;
+    struct node *ptr2=head;
+    pos--;
+    while(pos){
+       ptr2=ptr2->next;
+        pos--;
+    }
+    ptr1=ptr2->prev;
+    ptr1->next=temp;
+    temp->prev=ptr1;
+    temp->next=ptr2;
+    ptr2->prev=temp;
+}
+int main(){
+    add(45);
+    beginning(12);
+    InsertEnd(92);
+    AtPos(89, 3);
+    
+    struct node *ptr=head;
+    while(ptr!=NULL){
+        cout<<ptr->data<<endl;
+        ptr=ptr->next;
+    }
+    
+    return 0;
+}
--- LinkedList.cpp
+++ LinkedList.cpp
@@ -5,7 +5,10 @@ struct node{
     struct node *link;
 };
 struct node *head=NULL;
-void Insertion(int value){
+void insertion(){
+    int value;
+    cout<<"enter value";
+    cin>>value;
     struct node *temp=(struct node*)malloc(sizeof(struct node));
     temp->data=value;
     temp->link=NULL;
@@ -36,14 +39,48 @@ void traverse(){
     }
     
 }
+void reverse()
+    {
+        struct node *ptr=head, *prvs=NULL;
+        while(head!=NULL){
+            ptr=head->link;
+            head->link=prvs;
+            prvs=head;
+            head=ptr;
+        }
+        head=prvs;
+
+    }
 int main(){
-    Insertion(12);
-    Insertion(23);
-    Insertion(45);
-    Insertion(62);
-    Insertion(73);
-    Insertion(91);
-    Insertion(87);
-    traverse();
+  cout<<"-------------------LINKED_LIST---------------------\n";
+	cout<<"1-INSERTION\n2-DELETION\n3-ADD_IN_START/BETWEEN\n4-TRAVERSE\n5-LENGTH\n6-REVERSE\n7-Search\n8-EXIT\n\n";
+	int x;
+	while (1)
+	{
+	cout<<"Enter the operation you want to perform: ";
+	cin>>x;
+	
+		switch (x)
+		{
+		case 1: insertion();
+			break;
+		// case 2: dlt ();
+		// 	break;
+		// case 3: add_between();
+		// 	break;
+		case 4: traverse();
+			break;
+		// case 5: length();
+		// 	break;
+		case 6: reverse();
+			break;
+		// case 7:search();
+		// 	break;
+		case 8: cout<<"Exiting...........\n";
+			exit(1);
+		default: cout<<"CHOOSE FROM THE GIVEN OPTION..!!\n";
+			break;
+		}
     return 0;
+}
 }
